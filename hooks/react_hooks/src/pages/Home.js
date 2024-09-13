3245de9673c9c4721914c9d78468b66614ad19fc
DouglasReis <douglasamaralreis@outlook.com>
2024-09-13 20:19:12 -0300

    update

--- hooks/react_hooks/src/pages/Home.js
+++ hooks/react_hooks/src/pages/Home.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import HookUseState from '../components/HookUseState'
+import HookUseReducer from '../components/HookUseReducer'
+import HookUseEffect from '../components/HookUseEffect'
+import HookUseRef from '../components/HookUseRef';
+
+// useContext
+import { useContext } from "react";
+import { SomeContext } from '../components/HookUseContext';
+import HookUseCallback from '../components/HookUseCallback';
 
 const Home = () => {
+
+  const {contextValue} = useContext(SomeContext)
+
   return (
     <div>
-        <h1><HookUseState/></h1>
+        <HookUseState/>
+        <HookUseReducer/>
+        <HookUseEffect/>
+        <h2>useContext</h2>
+        <p>Valor do context: {contextValue}</p>
+        <hr />
+        <HookUseRef/>
+        <HookUseCallback />
     </div>
   )
 }

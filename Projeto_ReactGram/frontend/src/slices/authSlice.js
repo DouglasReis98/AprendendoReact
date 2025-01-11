bf852c4e938e5e89e970b6151c3fdfbc28263652
DouglasReis98 <y*********@gmail.com>
2025-01-11 11:58:09 -0300

    form_login_erro

--- Projeto_ReactGram/frontend/src/slices/authSlice.js
+++ Projeto_ReactGram/frontend/src/slices/authSlice.js
@@ -1,6 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import authService from "../services/authService";
-import { act } from "react";
 
 const user = JSON.parse(localStorage.getItem("user"));
 
@@ -30,6 +29,21 @@ export const register = createAsyncThunk(
 export const logout = createAsyncThunk("auth/logout", async () => {
   await authService.logout();
 })
+
+// Sign in an user
+export const login = createAsyncThunk(
+  "auth/login",
+  async (user, thunkAPI) => {
+    const data = await authService.login(user);
+
+    // check for errors
+    if (data.errors) {
+      return thunkAPI.rejectWithValue(data.errors[0]);
+    }
+
+    return data;
+  }
+);
 
 export const authSlice = createSlice({
   name: "auth",
@@ -64,7 +78,21 @@ export const authSlice = createSlice({
         state.error = null;
         state.user = null;
       })
-      ;
+      .addCase(login.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+        state.user = action.payload;
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+        state.user = null;
+      });
   },
 });
 

8e93a7d1a17da101d7c923ca3e0561d9fcc7334d
DouglasReis98 <y*********@gmail.com>
2025-02-01 21:46:04 -0300

    exibir_e_excluir_fotos

--- Projeto_ReactGram/frontend/src/slices/photoSlice.js
+++ Projeto_ReactGram/frontend/src/slices/photoSlice.js
@@ -27,6 +27,35 @@ export const publishPhoto = createAsyncThunk(
   }
 );
 
+// Get user photos
+export const getUserPhotos = createAsyncThunk(
+  "photo/userphotos",
+  async (id, thunkAPI) => {
+    const token = thunkAPI.getState().auth.user.token;
+
+    const data = await photoService.getUserPhotos(id, token);
+
+    return data;
+  }
+);
+
+// Delete a photo
+export const deletePhoto = createAsyncThunk(
+  "photo/delete",
+  async (id, thunkAPI) => {
+    const token = thunkAPI.getState().auth.user.token;
+
+    const data = await photoService.deletePhoto(id, token);
+
+    // Check for errors
+    if (data.errors) {
+      return thunkAPI.rejectWithValue(data.errors[0]);
+    }
+
+    return data;
+  }
+);
+
 export const photoSlice = createSlice({
   name: "Photo",
   initialState,
@@ -46,10 +75,40 @@ export const photoSlice = createSlice({
         state.success = true;
         state.error = null;
         state.photo = action.payload;
-        state.photos.unshift(state.photo)
-        state.message = "Foto publicada com sucesso!"
+        state.photos.unshift(state.photo);
+        state.message = "Foto publicada com sucesso!";
       })
       .addCase(publishPhoto.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+        state.photo = {};
+      })
+      .addCase(getUserPhotos.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(getUserPhotos.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+        state.photos = action.payload;
+      })
+      .addCase(deletePhoto.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(deletePhoto.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+
+        state.photos = state.photos.filter((photo) => {
+          return photo._id !== action.payload.id;
+        });
+
+        state.message = action.payload.message;
+      })
+      .addCase(deletePhoto.rejected, (state, action) => {
         state.loading = false;
         state.error = action.payload;
         state.photo = {};

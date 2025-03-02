51cf44da30891ce5d3ffbd6db2b63eaa291a7de3
DouglasReis98 <y*********@gmail.com>
2025-03-02 20:59:38 -0300

    typescript_introducao

---
+++ TypeScript/1_Intro/index.ts
@@ -0,0 +1,11 @@
+const firstName = "Douglas"
+const anotherName = 1
+const x = true
+
+function greeting(name: String) {
+    console.log("Olá, " + name)
+}
+
+greeting(firstName)
+//greeting(anotherName)
+//greeting(x)
